f7d9f3a91712eefaf33cae7a5dbcce3bd2eac7aa
Lihay Morad <44345425+LihayMorad@users.noreply.github.com>
2019-01-08 11:51:33 +0200

    env config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
   let config = {
-    apiKey: "AIzaSyAtq44pGpHzdrfBImF_ZDP8wiPW38RkE6w",
-    authDomain: "movies-to-watch-26077.firebaseapp.com",
-    databaseURL: "https://movies-to-watch-26077.firebaseio.com",
-    projectId: "movies-to-watch-26077",
-    storageBucket: "movies-to-watch-26077.appspot.com",
-    messagingSenderId: "932668361467"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   };
   firebase.initializeApp(config);
 
