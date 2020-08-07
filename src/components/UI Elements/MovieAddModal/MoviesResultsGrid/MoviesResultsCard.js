be6d0b8b0b68598b2ada00679ddf98c185e678f7
LihayM <LihayMorad@gmail.com>
2020-08-07 23:13:10 +0300

    export movies results card to a separate component

---
+++ src/components/UI Elements/MovieAddModal/MoviesResultsGrid/MoviesResultsCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+
+import youTubeIcon from '../../../../assets/youtube_icon.png';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const StyledTooltip = withStyles({ tooltip: { color: 'white', backgroundColor: 'black', fontSize: '12px' }, tooltipPlacementBottom: { marginTop: '-27px' } })(Tooltip);
+
+const moviesResultsCard = ({ id, isSelectedMovie, onClick, poster, hasValidPoster, title, year, onTrailerClick }) => {
+    return (
+        <StyledTooltip title="Click to add this movie" TransitionComponent={Zoom} key={id}>
+            <li className={`movieElem ${isSelectedMovie && 'chosenMovie'}`}
+                onClick={onClick}>
+                <img src={poster} alt={hasValidPoster ? "Movie poster" : "Movie poster not found"} />
+                <div className={`overlay ${!hasValidPoster && 'overlayBlack'}`}>
+                    <h2>{title}</h2>
+                    <h3>{year}</h3>
+                </div>
+                <img src={youTubeIcon} id="youTubeIcon" alt="YouTube icon" title="Click to watch the trailer"
+                    onClick={onTrailerClick} />
+            </li>
+        </StyledTooltip>
+    );
+};
+
+export default moviesResultsCard;
--- src/components/UI Elements/MovieAddModal/MoviesResultsGrid/MoviesResultsGrid.js
+++ src/components/UI Elements/MovieAddModal/MoviesResultsGrid/MoviesResultsGrid.js
@@ -1,63 +1,40 @@
 import React from 'react';
+import MoviesResultsItem from './MoviesResultsCard';
 
-import Tooltip from '@material-ui/core/Tooltip';
-import Zoom from '@material-ui/core/Zoom';
-
-import youTubeIcon from '../../../../assets/youtube_icon.png';
-
-import { withStyles } from '@material-ui/core/styles';
 import './MoviesResultsGrid.css';
-
-const StyledTooltip = withStyles({ tooltip: { color: 'white', backgroundColor: 'black', fontSize: '12px' }, tooltipPlacementBottom: { marginTop: '-27px' } })(Tooltip);
 
 const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500/";
 
-const moviesResultsGrid = props => {
+const moviesResultsGrid = ({ type, results, imdbID, tmdbID, updateCurrentMovie, toggleWatchTrailer, getIMDBID }) => {
     let moviesSearchResultList = null;
 
-    switch (props.type) {
+    switch (type) {
 
         case "search":
-            moviesSearchResultList = props.results.map(elem => {
-                const hasPoster = elem.Poster && elem.Poster !== "N/A";
-                const chosenMovie = props.imdbID === elem.imdbID;
-                return (
-                    <StyledTooltip title="Click to add this movie" TransitionComponent={Zoom} key={elem.imdbID}>
-                        <li className={`movieElem ${chosenMovie && 'chosenMovie'}`}
-                            onClick={() => { props.updateCurrentMovie(elem.imdbID, "", elem.Title, elem.Year); }}>
-                            <img src={elem.Poster} alt={hasPoster ? "Movie poster" : "Movie poster not found"} />
-                            <div className={`overlay ${!hasPoster && 'overlayBlack'}`}>
-                                <h2>{elem.Title}</h2>
-                                <h3>{elem.Year}</h3>
-                            </div>
-                            <img src={youTubeIcon} id="youTubeIcon" alt="YouTube icon" title="Click to watch the trailer"
-                                onClick={e => { e.stopPropagation(); props.toggleWatchTrailer(`${elem.Title} ${elem.Year}`, elem.imdbID); }} />
-                        </li>
-                    </StyledTooltip>
-                )
+            moviesSearchResultList = results.map(elem => {
+                const hasValidPoster = elem.Poster && elem.Poster !== "N/A";
+                const isSelectedMovie = imdbID === elem.imdbID;
+                return <MoviesResultsItem
+                    key={elem.imdbID} id={elem.imdbID} isSelectedMovie={isSelectedMovie}
+                    onClick={() => updateCurrentMovie(elem.imdbID, "", elem.Title, elem.Year)}
+                    poster={elem.Poster} hasValidPoster={hasValidPoster} title={elem.Title} year={elem.Year}
+                    onTrailerClick={e => { e.stopPropagation(); toggleWatchTrailer(`${elem.Title} ${elem.Year}`, elem.imdbID); }}
+                />
             });
             break;
 
         case "trending":
-            moviesSearchResultList = props.results.map(elem => {
-                const hasPoster = elem.poster_path && elem.poster_path !== "N/A";
-                const chosenMovie = props.tmdbID === elem.id;
+            moviesSearchResultList = results.map(elem => {
+                const hasValidPoster = elem.poster_path && elem.poster_path !== "N/A";
+                const isSelectedMovie = tmdbID === elem.id;
                 const releaseDate = elem.release_date && new Date(elem.release_date);
                 const year = releaseDate.getFullYear();
-                return (
-                    <StyledTooltip title="Click to add this movie" TransitionComponent={Zoom} key={elem.id}>
-                        <li className={`movieElem ${chosenMovie && 'chosenMovie'}`}
-                            onClick={() => { props.getIMDBID(elem.id, elem.title, year); }}>
-                            <img src={POSTER_BASE_URL + elem.poster_path} alt={hasPoster ? "Movie poster" : "Movie poster not found"} />
-                            <div className={`overlay ${!hasPoster && 'overlayBlack'}`}>
-                                <h2>{elem.title}</h2>
-                                <h3>{year}</h3>
-                            </div>
-                            <img src={youTubeIcon} id="youTubeIcon" alt="YouTube icon" title="Click to watch the trailer"
-                                onClick={e => { e.stopPropagation(); props.toggleWatchTrailer(`${elem.title} ${year}`, elem.id); }} />
-                        </li>
-                    </StyledTooltip>
-                )
+                return <MoviesResultsItem
+                    key={elem.id} isSelectedMovie={isSelectedMovie}
+                    onClick={() => { getIMDBID(elem.id, elem.title, year); }}
+                    poster={POSTER_BASE_URL + elem.poster_path} hasValidPoster={hasValidPoster} title={elem.title} year={year}
+                    onTrailerClick={e => { e.stopPropagation(); toggleWatchTrailer(`${elem.title} ${year}`, elem.id); }}
+                />
             });
             break;
 
